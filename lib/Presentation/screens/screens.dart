78a667306334caf64b4fecc0e5d6c90f810111f3
Edward Luis Huayllasco carlos <edwardhc1997@gmail.com>
2023-09-12 00:01:29 -0500

    screens creadas, aun vacias

--- lib/Presentation/screens/screens.dart
+++ lib/Presentation/screens/screens.dart
@@ -1,5 +1,9 @@
+export 'package:widgets_app/Presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/Presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/Presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/Presentation/screens/cards/card_screen.dart';
 export 'package:widgets_app/Presentation/screens/home/home_screen.dart';
-
-
+export 'package:widgets_app/Presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/Presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/Presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/Presentation/screens/ui_control/ui_control_screen.dart';
