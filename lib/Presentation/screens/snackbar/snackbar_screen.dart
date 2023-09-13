77251aef736f4028df90e182ec0c6f23b3cf9a7d
Edward Luis Huayllasco carlos <edwardhc1997@gmail.com>
2023-09-13 11:55:50 -0500

    snackbar finished

--- lib/Presentation/screens/snackbar/snackbar_screen.dart
+++ lib/Presentation/screens/snackbar/snackbar_screen.dart
@@ -4,8 +4,29 @@ class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
   const SnackbarScreen({super.key});
 
+  void showCustomSanckbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final snackback = SnackBar(
+      content: const Text('Hola mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: (){}),
+      duration: Duration(seconds: 2),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackback);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SnackScreen y dialogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showCustomSanckbar(context);
+        },
+        label: const Text('Mostrar SnackBar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+    );
   }
 }
--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -20,4 +20,5 @@ const List<MenuItem> appMenuItems =  [
   MenuItem(title: 'Botones', subtitle: 'Varios Botones en Flutter', link: '/buttons', icon: Icons.smart_button_outlined),
   MenuItem(title: 'Tarjeta', subtitle: 'Un contenedor estilizado', link: '/cards', icon: Icons.credit_card),
   MenuItem(title: 'ProgressIndicator', subtitle: 'Generales y controlados', link: '/progress', icon: Icons.refresh_rounded),
+  MenuItem(title: 'SnackBars and Dialogs', subtitle: 'Indicadores en pantalla', link: '/snackbars', icon: Icons.refresh_rounded),
 ];
